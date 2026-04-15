d3a72b79b3a10e18b0072da9fa6b5df483b3b066
jaovbcx <v.conde@academico.ifrr.edu.br>
2026-04-15 17:04:07 -0400

    hambuguer logos uploaded

---
+++ desafio-front-piz-web/src/components/TextPigz.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import hamburguerFoto from '../assets/hamburguer-with-floating-ingredients.png';
+
+function TextPigz() {
+  return (
+    <div className="texto">
+      <h2>Pigz: tudo que você precisa pra vender mais!</h2>
+      <img src='hamburguerFoto'></img>
+      <p>Temos uma equipe ansiosa para cadastrar seus produtos no Pigz Marketplace e deixar sua loja prontinha para iniciar as vendas.</p>
+    </div>
+  )
+}
+
+export default TextPigz;
--- desafio-front-piz-web/src/main.tsx
+++ desafio-front-piz-web/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Header } from './components/Header.tsx'
+import TextPigz from './components/TextPigz.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
      <Header />
+     <TextPigz />
   </StrictMode>,
 )
