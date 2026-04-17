cd6e32812576e126fea9f3625b94dd747a150b99
jaovbcx <v.conde@academico.ifrr.edu.br>
2026-04-17 16:00:02 -0400

    added all card logos

--- desafio-front-piz-web/src/components/Card.tsx
+++ desafio-front-piz-web/src/components/Card.tsx
@@ -8,9 +8,24 @@ const LogoImage = styled.img`
   width: 50%;
 `
 
+const Title = styled.h2`
+  font-family: 'Poppins';
+  font-weight: bold;
+`
+const CardContainer= styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Subtitles = styled.p`
+  font-family: 'Poppins';
+`
 
 export function Card() {
   return(
-      <LogoImage src='{marketplaceLogo}'>
+    <CardContainer>
+      <LogoImage src='{marketplaceLogo}'/>
+      <Title></Title>
+    </CardContainer>
   )
 }
